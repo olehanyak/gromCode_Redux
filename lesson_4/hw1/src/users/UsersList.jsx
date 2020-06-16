c17519544803a740089edaa93b8d0017f9418975
Oleh Hanyak <ohanyak@gmail.com>
2020-06-16 06:43:49 +0300

    add

---
+++ lesson_4/hw1/src/users/UsersList.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { connect } from "react-redux";
+import User from "./User";
+import Pagination from "./Pagination";
+import * as usersAction from "./users.actions";
+
+const UsersList = ({ users, currentPage, goNext, goPrev }) => {
+    return (
+        <div>
+            <Pagination goPrev={goPrev} goNext={goNext} currentPage={currentPage} totalItems={users.length} />
+            <ul className="users">
+                {users.slice(currentPage * 3, currentPage * 3 + 3).map((user) => (
+                    <User key={user.id} name={user.name} age={user.age} />
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+const mapState = (state) => {
+    return {
+        users: state.usersList,
+        currentPage: state.currentPage,
+    };
+};
+
+const mapDispatch = {
+    goNext: usersAction.goNextPage,
+    goPrev: usersAction.goPrevPage,
+};
+
+export default connect(mapState, mapDispatch)(UsersList);
